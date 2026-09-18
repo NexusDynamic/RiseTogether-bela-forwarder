5a2b39449baf0daeb9a4b322464df2cb999df010
zeyus <dev@zeyus.com>
2026-09-18 15:43:26 +0200

    added bela run script (just to keep params consistent)

---
+++ bela-run.sh
@@ -0,0 +1,2 @@
+#/bin/sh
+/root/Bela/projects/RiseTogether-bela-forwarder/RiseTogether-bela-forwarder --analog-channels 0 --digital-channels 16 --audio-input-gain 0 --line-out-level 0 --hp-level 0 --verbose --mute-speaker 1 --use-analog 0 --use-digital 1 --disable-led --stop-button-pin -1 --period 16 --high-performance-mode
